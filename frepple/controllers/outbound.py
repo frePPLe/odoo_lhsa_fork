d86c5b5e1948442ce76b9d063f2ab458a8b38fd4
hichamlahlou <hlahlou@frepple.com>
2025-12-02 16:19:04 +0100

    Time zone is Buenos Aires

--- frepple/controllers/outbound.py
+++ frepple/controllers/outbound.py
@@ -27,9 +27,10 @@ import logging
 import pytz
 import xmlrpc.client
 from xml.sax.saxutils import quoteattr
-from datetime import datetime, timedelta
+from datetime import date, datetime, timedelta, time, timezone
 from pytz import timezone
 import ssl
+from zoneinfo import ZoneInfo
 
 try:
     import odoo
@@ -459,9 +460,21 @@ class exporter(object):
         )
 
     def formatDateTime(self, d, tmzone=None):
-        if not isinstance(d, datetime):
-            d = datetime.fromisoformat(d)
-        return d.astimezone(timezone(tmzone or self.timezone)).strftime(self.timeformat)
+        # 1. Ensure d is a datetime
+        if isinstance(d, date) and not isinstance(d, datetime):
+            # A pure date: treat as midnight UTC and no conversion
+            dt_midnight = datetime.combine(d, time(0, 0))
+            return dt_midnight.strftime(self.timeformat)
+
+        # 2. Attach UTC tzinfo (now it's aware UTC)
+        d = d.replace(tzinfo=timezone.utc)
+
+        # 3. Convert to target tz
+        tz = ZoneInfo(tmzone or "America/Argentina/Buenos_Aires")
+        d_local = d.astimezone(tz)
+
+        # 4. Format
+        return d_local.strftime(self.timeformat)
 
     def export_users(self):
         users = []
