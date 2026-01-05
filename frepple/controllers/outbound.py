0be0da36029b1a04cee0f11384717111ce45ca13
hichamlahlou <hlahlou@frepple.com>
2026-01-05 23:53:21 +0100

    remove ready for frepple

--- frepple/controllers/outbound.py
+++ frepple/controllers/outbound.py
@@ -1947,7 +1947,7 @@ class exporter(object):
             #     "operation_change",  # consumed in operation
             # ],
             search=[
-                ("eco_id.stage_id.ready_for_frepple", "=", True),
+                #  ("eco_id.stage_id.ready_for_frepple", "=", True),
                 ("eco_id.new_bom_id.active", "=", False),
             ],
             object=True,
